d3c97f4d75a310b4c96b7e4e607633dbdc57f541
Forpee <khalilhassam3@gmail.com>
2022-06-24 19:34:57 +0200

    a

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -8,11 +8,13 @@ void main()
     vec3 b=vBarycentric;
     float width=.01;
     
-    float borderx=max(step(vUv.x,width),step(1.-vUv.x,width));
-    float bordery=max(step(vUv.y,width),step(1.-vUv.y,width));
-    float border=max(borderx,bordery);
+    // float borderx=max(step(vUv.x,width),step(1.-vUv.x,width));
+    // float bordery=max(step(vUv.y,width),step(1.-vUv.y,width));
+    // float border=max(borderx,bordery);
+    
+    float border=max(max(step(b.x,width),step(b.y,width)),step(b.z,width));
     vec3 color=vec3(border);
     
     gl_FragColor=vec4(vUv,1.,1.);
-    gl_FragColor=vec4(vBarycentric,1.);
+    gl_FragColor=vec4(color,1.);
 }
