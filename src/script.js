997c1c4e7c61b6fd5469306fc999c00d8baf1368
Forpee <khalilhassam3@gmail.com>
2022-06-24 20:20:53 +0200

    a

--- src/script.js
+++ src/script.js
@@ -7,18 +7,42 @@ import fragmentShader from './shaders/fragment.glsl';
 import gsap from 'gsap';
 import * as OIMO from 'oimo';
 
-console.log(OIMO);
+// console.log(OIMO);
 /**
  * Base
  */
 // Debug
+let params = {
+    createBody: function () {
+        createBody();
+    }
+};
+let mouse = new THREE.Vector2();
+let point = new THREE.Vector3(0, 0, 0);
 const gui = new dat.GUI();
 
+gui.add(params, 'createBody');
 // Canvas
 const canvas = document.querySelector('canvas.webgl');
 
 // Scene
 const scene = new THREE.Scene();
+let raycaster = new THREE.Raycaster();
+
+let testPlane = new THREE.Mesh(new THREE.PlaneGeometry(100, 100), new THREE.MeshBasicMaterial({ color: 0x0000ff }));
+
+window.addEventListener('mousemove', e => {
+    mouse.x = (e.clientX / window.innerWidth) * 2 - 1;
+    mouse.y = -(e.clientY / window.innerHeight) * 2 + 1;
+
+    raycaster.setFromCamera(mouse, camera);
+    let intersects = raycaster.intersectObjects([testPlane]);
+    if (intersects.length > 0) {
+        // console.log(intersects[0].point);
+        point.copy(intersects[0].point);
+
+    }
+});
 
 let world = new OIMO.World({
     timestep: 1 / 60,
@@ -27,7 +51,7 @@ let world = new OIMO.World({
     worldscale: 1,
     random: true,
     info: false,
-    gravity: [0, -9.8, 0],
+    gravity: [0, 0, 0],
 });
 
 let body = world.add({
@@ -40,6 +64,7 @@ let body = world.add({
     friction: 0.5,
     restitution: 0.5,
     belongsTo: 1,
+    noSleep: true,
     collidesWith: 0xffffffff,
 });
 
@@ -50,7 +75,7 @@ function createBody() {
     let o = {};
 
     let body = world.add({
-        type: 'sphere',
+        type: 'box',
         size: [1, 1, 1],
         pos: [0, 0, 0],
         rot: [0, 0, 90],
@@ -64,13 +89,22 @@ function createBody() {
 
     o.body = body;
     o.mesh = new THREE.Mesh(new THREE.BoxBufferGeometry(1, 1, 1),
-        new THREE.MeshBasicMaterial({ color: 0xffffff })
+        new THREE.MeshBasicMaterial({ color: 0x0000ff })
     );
     bodies.push(o);
+    scene.add(o.mesh);
 }
 // ground
 
 let ground = world.add({ size: [40, 1, 40], pos: [0, -4, 0], world: world });
+let ground1 = world.add({ size: [40, 1, 40], pos: [0, 4, 0], world: world });
+
+let left = world.add({ size: [1, 40, 40], pos: [-4, 0, 0], world: world });
+let right = world.add({ size: [1, 40, 40], pos: [4, 0, 0], world: world });
+
+let z = world.add({ size: [40, 40, 1], pos: [0, 0, -2], world: world });
+let z1 = world.add({ size: [40, 40, 1], pos: [0, 0, 2], world: world });
+
 /**
  * Test mesh
  */
@@ -158,8 +192,16 @@ const clock = new THREE.Clock();
 const tick = () => {
     // Update controls
     world.step();
+    body.awake();
+    body.setPosition(point);
     mesh.position.set(body.position.x, body.position.y, body.position.z);
     mesh.quaternion.set(body.quaternion.x, body.quaternion.y, body.quaternion.z, body.quaternion.w);
+
+    bodies.forEach(o => {
+        o.mesh.position.set(o.body.position.x, o.body.position.y, o.body.position.z);
+        o.mesh.quaternion.set(o.body.quaternion.x, o.body.quaternion.y, o.body.quaternion.z, o.body.quaternion.w);
+    }
+    );
     controls.update();
 
     // Get elapsedtime
