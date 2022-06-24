e908b769c390f996e599c0890c6967f2a895d7e4
Forpee <khalilhassam3@gmail.com>
2022-06-24 19:40:37 +0200

    a

--- src/script.js
+++ src/script.js
@@ -94,7 +94,8 @@ const renderer = new THREE.WebGLRenderer({
 });
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-
+// antialias
+renderer.antialias = true;
 /**
  * Animate
  */
