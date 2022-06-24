61e3a66103844e6f3e2a67f1ee853c653c9f55c2
Forpee <khalilhassam3@gmail.com>
2022-06-24 19:29:54 +0200

    a

--- src/shaders/vertex.glsl
+++ src/shaders/vertex.glsl
@@ -1,10 +1,13 @@
 varying vec2 vUv;
 
+attribute vec3 barycentric;
+
+varying vec3 vBarycentric;
 void main()
 {
     // vec4 mvPosition=modelViewMatrix*vec4(position,1.);
     // gl_PointSize=100.*(1./-mvPosition.z);
-
+    
     gl_Position=projectionMatrix*modelViewMatrix*vec4(position,1.);
     
     vUv=uv;
